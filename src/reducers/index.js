3934dfce54131c2ad68c779599fd261225ff28c5
Jeffrey Liu <jeffkliu@gmail.com>
2020-03-13 13:02:36 -0400

    added combineReducers from redux, then exported combineReducers for songs: songsReducer and selectedSong: selectedSongReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
       {title: 'No Scrubs', duration: '4:05'},
@@ -8,10 +10,15 @@ const songsReducer = () => {
 };
 
 
-const selectedSongReducer = (selectedSong =null, action) => {
+const selectedSongReducer = (selectedSong = null, action) => {
    if(action.type === 'SONG_SELECTED'){
       return action.payload;
    }
 
    return selectedSong;
 };
+
+export default combineReducers({
+   songs: songsReducer,
+   selectedSong: selectedSongReducer
+});
